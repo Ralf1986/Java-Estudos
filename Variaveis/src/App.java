488e2d0b195158be206cca7e744fad123e4e4158
Ralf1986 <ralfsantos.oliveira@gmail.com>
2024-09-24 22:08:43 -0300

    detalhes de variaveis

--- Variaveis/src/App.java
+++ Variaveis/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // Casting
+
+        final int NUMERO = 1; // constante
+        final double NUMERO_DE_PI = 3.14; // constante letras maiuscula
+
+        double salarioMinimo = 2500;
+        Float pi = 3.14F;
+        long cpf = 34554654373l;
+
         System.out.println("Hello, World!");
     }
 }
